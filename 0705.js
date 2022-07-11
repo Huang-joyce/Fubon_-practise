f5f883662c4b47b3e606322bd4e156eccce2f314
joyce <joyce890926@gmail.com>
2022-07-11 11:04:28 +0800

    0711

--- 0705.js
+++ 0705.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
     $('.content__row3__h3__img').click(function(){
         $('.content__row3__ol').toggle();
         $('.content__row3__h3__img').toggleClass("content__row3__h3__img--reverse");
